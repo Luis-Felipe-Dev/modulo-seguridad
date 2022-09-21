6412ec52ac204ce9b8d5630bbd7022577767f9ef
Luis Felipe <luis.fhb.2016@gmail.com>
2022-09-21 08:18:09 -0500

    fisrt change

---
+++ src/main/java/pe/isil/moduloseguridad/Application.java
@@ -0,0 +1,13 @@
+package pe.isil.moduloseguridad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+
+        System.out.println("Hello world!!!");
+    }
+}
